9d75407125033c4cf13823c794c8492275545ddd
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-08-01 07:56:12 -0300

    exercico bonus numero 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,9 +4,19 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+function triangleCheck(lineA, lineB, lineC) {
+  let formaTriangulo = false;
+
+  if (lineA < Math.abs(lineB + lineC) && lineA > Math.abs(lineB - lineC)) {
+    formaTriangulo = true; 
+  } else if (lineB < Math.abs(lineA + lineC) && lineB > Math.abs(lineA - lineC)){
+    formaTriangulo = true; 
+  } else if (lineC < Math.abs(lineA + lineB) && lineC > Math.abs(lineA - lineB)) {
+    formaTriangulo = true; 
+  }
+  return formaTriangulo;
+  }
+  //console.log(triangleCheck(23,10,10))
 
 // Desafio 13
 function hydrate() {
